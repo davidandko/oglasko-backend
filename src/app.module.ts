fcdd607b3f57389128478b65cc2046cce49202f8
davidandko <davidandko.ai@gmail.com>
2023-12-28 17:11:57 +0100

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+@Module({
+  imports: [ProductsModule,MongooseModule.forRoot('mongodb+srv:mongodb+srv://davidandko:password@nestjs-excercise.3hgi1bb.mongodb.net/?retryWrites=true&w=majority')],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
